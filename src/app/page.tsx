1359dd1a772005805a148fbe074662ead7791a04
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-01-04 22:43:04 +0900

    タイトルをクリックでルートページに移動するように修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { GetData } from '@/app/_components/GetData'
 import styles from '@/app/page.module.css'
+import '@/styles/variables.css'
+import Link from 'next/link'
 import { FC } from 'react'
 
 const Home: FC = () => {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Atcoder Random</h1>
+      <Link href='/' className={styles.title}>
+        <h1>Atcoder Random</h1>
+      </Link>
       <GetData />
     </main>
   )
